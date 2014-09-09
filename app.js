64bcb7277f53fba5084345bcd5ad733ea5863896
Brian Mitchell <bman4789@gmail.com>
2014-09-09 11:41:45 -0500

    Added some Angular

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.locals.delimiters = '<% %>';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- public/javascripts/placeholder.js
+++ public/javascripts/placeholder.js
@@ -1,1 +1,46 @@
-//This is a placeholder!
+function Wordlist(){
+    this.name = "list";
+    this.words = [];
+    this.includeInCurrent= true;
+}
+
+Wordlist.prototype.isEmpty = function () {
+    return this.words.length == 0;
+};
+Wordlist.prototype.add = function (thing) {
+    this.words.push(thing);
+};
+Wordlist.prototype.join = function (otherList) {
+    for (var i=0; i<otherList.words.length; i++) {
+        this.add(otherList.words[i]);
+        console.log(otherList.words[i]);
+    }
+};
+Wordlist.prototype.find = function (thing) {
+    var index = -1;
+    for (var i=0; i<this.words.length; i++) {
+        if (this.words[i]==thing) {
+            console.log("The item " + thing + " was found at location " + i);
+            return i;
+        }
+    }
+    return index;
+};
+Wordlist.prototype.remove = function (thing) {
+    var index = Math.max(-1, this.find(thing));
+    while (index != -1) {
+        this.words.splice(index, 1);
+        index = Math.max(-1, this.find(thing));
+    }
+};
+Wordlist.prototype.updateName = function(name) {
+    this.name = name;
+};
+
+//---------------------------------------------------------------------------------------------
+
+var wordlist1 = new Wordlist();
+wordlist1.add("bird");
+wordlist1.add("elephant");
+
+console.log(wordlist1);
