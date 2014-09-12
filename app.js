7ab6b842b094b9bfc95d16502cfab5299419c9ac
Brian Mitchell <bman4789@gmail.com>
2014-09-12 00:03:10 -0500

    Removed template engine and routing. This simplifies the project significantly and makes for a good easy base for basic node and express development.

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var http    = require('http');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
-app.locals.delimiters = '<% %>';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine("html", require("hogan-express"));
-app.set('view engine', 'html');
-
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'models')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
