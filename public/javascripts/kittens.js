ff576e4151c7cefd35845f7868a3a84eac984478
Brian Mitchell <bman4789@gmail.com>
2014-09-14 22:25:38 -0500

    Added new page with more Angular examples using angular-ui. Also added some styling and bootstrap!

---
+++ public/javascripts/kittens.js
@@ -0,0 +1,38 @@
+angular.module('kittensApp', ['ui.bootstrap']);
+function CarouselDemoCtrl($scope) {
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+        var newWidth = 500 + slides.length;
+        slides.push({
+            image: 'http://placekitten.com/' + newWidth + '/300',
+            text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+                ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+        });
+    };
+    for (var i=0; i<4; i++) {
+        $scope.addSlide();
+    }
+}
+
+function DropdownCtrl($scope) {
+    $scope.items = [
+        'The first choice!',
+        'And another choice for you.',
+        'but wait! A third!'
+    ];
+
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function(open) {
+        console.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+}
